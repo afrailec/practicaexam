3e12d529c86c39d7f2d89d557c39b9f61a40608e
Alvaro <afrailecarmena@gmail.com>
2021-01-17 18:37:25 +0100

    Random Matrix Gen & Basic Print Matrix

--- practicasecam/main.c
+++ practicasecam/main.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#define N 10
+
+void inicializar_matriz(int matriz[N][N]);
+void rellenar_matriz(int matriz[N][N]);
+int random_number();
+void imprimir_matriz();
 
 int main()
 {
-    printf("Hello world!\n");
+    srand(time(NULL));
+
+    int matriz[N][N];
+
+    inicializar_matriz(matriz);
+    rellenar_matriz(matriz);
+    imprimir_matriz(matriz);
+
     return 0;
+}
+
+void inicializar_matriz(int matriz[N][N]){
+    int i,j;
+
+    for(i=0; i<=N; i++){
+        for(j=0; j<=N; j++){
+            matriz[i][j]=0;
+        }
+    }
+}
+
+void rellenar_matriz(int matriz[N][N]){
+    int i,j;
+
+    for(i=0; i<=N; i++){
+        for(j=0; j<=N; j++){
+            matriz[i][j]=random_number();
+        }
+    }
+}
+
+int random_number(){
+    return rand()%10;
+}
+
+void imprimir_matriz(int matriz[N][N]){
+    int i,j;
+
+    for(i=0; i<=N; i++){
+        for(j=0; j<=N; j++){
+            printf("%d ",matriz[i][j]);
+        }
+        printf("\n");
+    }
 }
